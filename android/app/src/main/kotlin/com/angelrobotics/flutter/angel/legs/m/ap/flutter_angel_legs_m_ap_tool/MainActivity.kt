2e879c7be96ef227ecc9595b3819c9b0c72c8a51
JAICHANG.PARK <jaichang@angel-robotics.com>
2021-05-03 09:54:12 +0900

    [Add] First Commit

---
+++ android/app/src/main/kotlin/com/angelrobotics/flutter/angel/legs/m/ap/flutter_angel_legs_m_ap_tool/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.angelrobotics.flutter.angel.legs.m.ap.flutter_angel_legs_m_ap_tool
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '엔젤렉스M 통신모듈 툴',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: '엔젤렉스M 통신모듈 툴'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, this.title}) : super(key: key);
+
+  final String? title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title!),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+           ListTile(
+             title: Text("펌웨어 업데이트"),
+             subtitle: Text("통신모듈 펌웨어를 업데이트 합니다."),
+           ),
+              Text(
+                '$_counter',
+                style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
