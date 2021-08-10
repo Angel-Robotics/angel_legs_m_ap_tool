087a193223f556701ae746e8f30be972380b48fd
Dreamwalker <aristojeff@gmail.com>
2021-08-10 12:08:12 +0900

    [Update] Maintain Packages

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:permission_handler/permission_handler.dart';
-
 import 'ui/app_main.dart';
 
-void main() {
-  runApp(AppMain());
-}
+void main() =>runApp(AppMain());
--- lib/ui/app_main.dart
+++ lib/ui/app_main.dart
@@ -3,14 +3,12 @@ import 'package:get/get.dart';
 import 'tool_home_page.dart';
 
 class AppMain extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
       title: '엔젤렉스M 통신모듈 툴',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.blue),
       home: ToolHomePage(title: '엔젤렉스M 통신모듈 툴'),
     );
   }
